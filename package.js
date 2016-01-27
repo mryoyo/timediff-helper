14c287024030648d804549709a6cec4642fba93e
Suriya[on meteor@183.90.168.108] <suriya.k@appman.co.th>
2016-01-27 20:38:21 +0700

    fixed dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mryoyo:timediff',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'A simple helper that displays duration between now and your another timestamp reactively.',
   git: 'https://github.com/mryoyo/timediff-helper.git',
   documentation: 'README.md'
@@ -8,6 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
+  api.use('session', ['client']);
   api.use('templating', ['client']);
   api.use('momentjs:moment@2.10.6', ['client']);
   api.addFiles('timediff.js', 'client');
